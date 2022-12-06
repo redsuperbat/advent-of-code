f5efaddf911a42022a468fe8bb093cbb1a277aa3
Max Netterberg <max.netterberg@gmail.com>
2022-12-06 21:19:33 +0100

    Part 2 is solved

--- 2022/1/main.rs
+++ 2022/1/main.rs
@@ -1,19 +1,32 @@
 use std::fs;
 
+fn get_max_and_remove(vec: &mut Vec<u32>) -> u32 {
+    let max = *vec.iter().max().unwrap();
+    let index = vec.iter().position(|it| *it == max).unwrap();
+    vec.remove(index);
+    max
+}
+
 fn main() {
     let input = fs::read("input.txt").unwrap();
-    let calories = input
+    let mut calories = input
         .iter()
         .map(|it| *it as char)
         .collect::<String>()
         .split("\n\n")
+        .filter(|it| !it.is_empty())
         .map(|it| {
             it.split('\n')
                 .map(|it| it.parse::<u32>().unwrap_or(0))
                 .sum::<u32>()
         })
-        .max()
-        .unwrap();
+        .collect::<Vec<u32>>();
 
-    println!("{:?}", calories)
+    let mut total: u32 = 0;
+    for idx in 1..4 {
+        let max = get_max_and_remove(&mut calories);
+        total += max;
+        println!("Max{}: {}", idx, max)
+    }
+    println!("Total: {}", total);
 }
--- 2022/2/main.rs
+++ 2022/2/main.rs
@@ -1,55 +1,112 @@
 use phf::phf_map;
 use std::fs;
 
+#[derive(Debug)]
 enum GameInput {
-    Rock(),
-    Paper(),
-    Scissors(),
+    Rock,
+    Paper,
+    Scissors,
+}
+
+#[derive(Debug)]
+enum Outcome {
+    Win,
+    Loss,
+    Draw,
 }
 
 impl GameInput {
     fn battle_against(&self, opponent: &GameInput) -> u32 {
         match self {
-            GameInput::Rock() => match opponent {
-                GameInput::Rock() => 3 + 1,
-                GameInput::Paper() => 1,
-                GameInput::Scissors() => 6 + 1,
+            GameInput::Rock => match opponent {
+                GameInput::Rock => 3 + 1,
+                GameInput::Paper => 1,
+                GameInput::Scissors => 6 + 1,
             },
-            GameInput::Paper() => match opponent {
-                GameInput::Rock() => 2,
-                GameInput::Paper() => 3 + 2,
-                GameInput::Scissors() => 6 + 2,
+            GameInput::Paper => match opponent {
+                GameInput::Rock => 6 + 2,
+                GameInput::Paper => 3 + 2,
+                GameInput::Scissors => 2,
             },
-            GameInput::Scissors() => match opponent {
-                GameInput::Rock() => 3,
-                GameInput::Paper() => 6 + 3,
-                GameInput::Scissors() => 3 + 3,
+            GameInput::Scissors => match opponent {
+                GameInput::Rock => 3,
+                GameInput::Paper => 6 + 3,
+                GameInput::Scissors => 3 + 3,
             },
         }
     }
 }
 
-static MAP: phf::Map<&'static str, GameInput> = phf_map! {
-    "A" => GameInput::Rock(),
-    "B" => GameInput::Paper(),
-    "C" => GameInput::Scissors(),
-    "Y" => GameInput::Rock(),
-    "X" => GameInput::Paper(),
-    "Z" => GameInput::Scissors()
+impl Outcome {
+    fn get_required_input(&self, match_against: &GameInput) -> GameInput {
+        match self {
+            Outcome::Win => match match_against {
+                GameInput::Rock => GameInput::Paper,
+                GameInput::Paper => GameInput::Scissors,
+                GameInput::Scissors => GameInput::Rock,
+            },
+            Outcome::Loss => match match_against {
+                GameInput::Rock => GameInput::Scissors,
+                GameInput::Paper => GameInput::Rock,
+                GameInput::Scissors => GameInput::Paper,
+            },
+            Outcome::Draw => match match_against {
+                GameInput::Rock => GameInput::Rock,
+                GameInput::Paper => GameInput::Paper,
+                GameInput::Scissors => GameInput::Scissors,
+            },
+        }
+    }
+}
+
+static PART_ONE_MAP: phf::Map<&'static str, GameInput> = phf_map! {
+    "A" => GameInput::Rock,
+    "B" => GameInput::Paper,
+    "C" => GameInput::Scissors,
+    "X" => GameInput::Rock,
+    "Y" => GameInput::Paper,
+    "Z" => GameInput::Scissors
+};
+
+static PART_TWO_MAP: phf::Map<&'static str, Outcome> = phf_map! {
+    "X" => Outcome::Loss,
+    "Y" => Outcome::Draw,
+    "Z" => Outcome::Win
 };
 
 fn main() {
+    // Part one
     let input = fs::read_to_string("input.txt").unwrap();
     let result = input
         .split('\n')
         .map(|it| it.split_once(' ').unwrap())
         .map(|it| {
-            let (a, b) = (MAP.get(it.0).unwrap(), MAP.get(it.1).unwrap());
-            println!("Battling {} vs {}", it.0, it.1);
-            let outcome = b.battle_against(a);
-            println!("Outcome is {}", outcome);
-            outcome
+            let (opponent, me) = (
+                PART_ONE_MAP.get(it.0).unwrap(),
+                PART_ONE_MAP.get(it.1).unwrap(),
+            );
+            me.battle_against(opponent)
         })
         .sum::<u32>();
-    println!("{}", result)
+    println!("Part one: {}", result);
+
+    // Part two
+    let result = input
+        .split('\n')
+        .map(|it| it.split_once(' ').unwrap())
+        .map(|it| {
+            let (opponent, determined_outcome) = (
+                PART_ONE_MAP.get(it.0).unwrap(),
+                PART_TWO_MAP.get(it.1).unwrap(),
+            );
+            println!(
+                "Opponent {:?}, Determined outcome {:?}",
+                opponent, determined_outcome
+            );
+            let me = determined_outcome.get_required_input(opponent);
+            println!("My input {:?}", me);
+            me.battle_against(opponent)
+        })
+        .sum::<u32>();
+    println!("Part two: {}", result);
 }
