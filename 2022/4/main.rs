6b6ac9f4ea8ce6490f6ce656aeee849224fdcf96
Max Netterberg <max.netterberg@gmail.com>
2022-12-06 22:15:10 +0100

    Completed part 1 🚀

---
+++ 2022/4/main.rs
@@ -0,0 +1,41 @@
+use std::fs;
+
+struct Range {
+    low: u32,
+    high: u32,
+}
+
+impl Range {
+    fn contains(&self, r2: &Range) -> bool {
+        self.low <= r2.low && self.high >= r2.high
+    }
+
+    fn from_str(string: &str) -> Range {
+        let (low, high) = string.split_once('-').unwrap();
+        Range {
+            low: low.parse::<u32>().unwrap(),
+            high: high.parse::<u32>().unwrap(),
+        }
+    }
+}
+
+fn part_one() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let res: u32 = input
+        .split('\n')
+        .map(|it| it.split_once(',').unwrap())
+        .map(|(first, second)| (Range::from_str(first), Range::from_str(second)))
+        .map(|(first, second)| u32::from(first.contains(&second) || second.contains(&first)))
+        .sum();
+
+    println!("{:?}", res)
+}
+
+fn part_two() {
+    let input = fs::read_to_string("input.txt").unwrap();
+}
+
+fn main() {
+    part_one();
+    part_two();
+}
