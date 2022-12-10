2c1c64df06a07db9583f6ca3d93fecfa0d6f5e04
Max Netterberg <max.netterberg@gmail.com>
2022-12-10 13:32:31 +0100

    Day 5 done

--- 2022/5/main.rs
+++ 2022/5/main.rs
@@ -42,35 +42,59 @@ fn parse_line(str: &str) -> (usize, usize, usize) {
     (vec[0], vec[1] - 1, vec[2] - 1)
 }
 
-fn run_sim(state: &mut [String; 9], instruction: &str) {
+fn run_sim_1(state: &mut [String; 9], instruction: &str) {
     let (amount, from, to) = parse_line(instruction);
     let m_boxes = state[from].get(0..amount).unwrap().to_string();
     state[from] = state[from].get(amount..).unwrap().to_string();
+
     for char in m_boxes.chars() {
         state[to].insert_str(0, &char.to_string())
     }
 }
 
-fn part_one() {
+fn run_sim_2(state: &mut [String; 9], instruction: &str) {
+    let (amount, from, to) = parse_line(instruction);
+    let m_boxes = state[from].get(0..amount).unwrap().to_string();
+    state[from] = state[from].get(amount..).unwrap().to_string();
+
+    // Part 2
+    state[to].insert_str(0, &m_boxes);
+}
+
+fn part_one() -> String {
     let input = fs::read_to_string("input.txt").unwrap();
     let (init_string, program_instructions) = input.split_once("\n\n").unwrap();
 
     let mut state = init_state(init_string);
     for (idx, line) in program_instructions.lines().enumerate() {
         print!("Iteration: {} value: {:?} -> ", idx + 1, state);
-        run_sim(&mut state, line);
+        run_sim_1(&mut state, line);
         println!("{:?}", state);
     }
-    for item in state {
-        print!("{}", item.chars().next().unwrap());
-    }
+    state
+        .iter()
+        .map(|it| it.chars().next().unwrap())
+        .collect::<String>()
 }
 
-fn part_two() {
+fn part_two() -> String {
     let input = fs::read_to_string("input.txt").unwrap();
+    let (init_string, program_instructions) = input.split_once("\n\n").unwrap();
+
+    let mut state = init_state(init_string);
+    for (idx, line) in program_instructions.lines().enumerate() {
+        print!("Iteration: {} value: {:?} -> ", idx + 1, state);
+        run_sim_2(&mut state, line);
+        println!("{:?}", state);
+    }
+    state
+        .iter()
+        .map(|it| it.chars().next().unwrap())
+        .collect::<String>()
 }
 
 fn main() {
-    part_one();
-    part_two();
+    let part1 = part_one();
+    let part2 = part_two();
+    println!("Answer part 1: {}, 2: {}", part1, part2)
 }
